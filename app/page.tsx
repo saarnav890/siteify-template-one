30afb2a9c33a40cbbf47a6e46b8392c510f26a93
bsaarnav <aarnavsanghvi890@gmail.com>
2023-02-19 06:06:03 -0800

    another one

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+
 import Navbar from '@/components/Navbar'
 import Main from '@/components/Main'
 import About from '@/components/About'
 import data from '../data.json';
 import CardsArea from '@/components/CardsArea'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 
 
