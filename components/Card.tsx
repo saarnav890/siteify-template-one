038dc738cf4b0159b20636191a12414e6943fcc0
bsaarnav <aarnavsanghvi890@gmail.com>
2023-02-19 08:31:16 -0800

    done

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,10 +15,10 @@ export default function Card({title, description, url, stars, language}: i) {
 
     return (
         <Link href={url}>
-     <div className="bg-white rounded-xl h-[24rem] w-96 flex flex-col hover:scale-105 active:scale-90 duration-200"
+     <div className="bg-white rounded-xl h-[24rem] w-96 flex flex-col hover:scale-105 active:scale-90 duration-200 pb-8"
      >
         
-        <div className="underline flex justify-center text-gray-900 text-2xl font-bold mt-6 ">
+        <div className="underline flex justify-center text-gray-900 text-4xl font-bold mt-12 ">
             {title}
         </div>
         <div className="flex justify-self-end mt-48  self-center p-4">
