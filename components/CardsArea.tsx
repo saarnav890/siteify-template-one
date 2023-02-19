b03ccdf51070aec22e6f4dc0832d8172abdda75a
bsaarnav <aarnavsanghvi890@gmail.com>
2023-02-19 08:34:18 -0800

    CIMPILES

--- components/CardsArea.tsx
+++ components/CardsArea.tsx
@@ -13,9 +13,9 @@ interface i{
 export default function CardsArea({projects}:i) {
     return (
         <div className="flex  justify-between w-full px-20 gap-10 flex-wrap mt-16 py-10">
-            {projects.map( (project) => {
+            {projects.map( (project, index) => {
                 return (
-                <Card title={project.title} description={project.description} stars={project.stars} language={project.language} url={project.url}/>
+                <Card key={index} title={project.title} description={project.description} stars={project.stars} language={project.language} url={project.url}/>
                 )
             })}
         </div>
